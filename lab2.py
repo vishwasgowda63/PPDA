73183008edbe34d7271e5da762bd176732472f5b
vishwasgowda63 <vishwasgowda6363@gmail.com>
2025-02-28 10:37:54 +0530

    lab2.py

---
+++ lab2.py
@@ -0,0 +1,10 @@
+import lab1 as np
+
+d1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+d2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+
+d3 = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print("Shape of 1D array:", d1.shape)
+print("Shape of 2D array:", d2.shape)
+print("Shape of 3D array:", d3.shape)
